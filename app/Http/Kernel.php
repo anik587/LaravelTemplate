bba4fb28def0d28cec8795ff9a3a24d8bdbdac78
anik.shojib <anik.shojib@reddotdigitalit.com>
2023-05-09 15:55:17 +0600

    spatie permission in progress

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,6 +63,10 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'password_expired' => \App\Http\Middleware\Password_expired::class
+        'password_expired' => \App\Http\Middleware\Password_expired::class,
+        'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
+        'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
+        'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
+    
     ];
 }
